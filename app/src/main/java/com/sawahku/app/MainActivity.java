fede1e843a45de4592d0e95e266e0def3fb72a52
irsyaadulibaaad-sketch <irsyaadulibaaad@gmail.com>
2026-05-08 23:20:48 +0700

    Add main activity

---
+++ app/src/main/java/com/sawahku/app/MainActivity.java
@@ -0,0 +1,42 @@
+package com.sawahku.app;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    
+    private WebView webView;
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        webView = findViewById(R.id.webView);
+        
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setDatabaseEnabled(true);
+        settings.setCacheMode(WebSettings.LOAD_DEFAULT);
+        settings.setAllowFileAccess(true);
+        settings.setAllowContentAccess(true);
+        
+        webView.setWebViewClient(new WebViewClient());
+        
+        // Load aplikasi SawahKu dari folder assets
+        webView.loadUrl("file:///android_asset/www/index.html");
+    }
+    
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
